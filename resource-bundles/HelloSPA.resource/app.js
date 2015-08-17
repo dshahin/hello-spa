4eb84329a9b946189b23a4e29cb45669210c6c9e
Dan Shahin <dan@codescience.com>
2015-08-16 22:53:40 -0700

    add controller and single jsr call

--- resource-bundles/HelloSPA.resource/app.js
+++ resource-bundles/HelloSPA.resource/app.js
@@ -1,7 +1,45 @@
 (function() {
 
-   var userName = configSettings.firstName + ' ' + configSettings.lastName;
+   var userName = $SPA.firstName + ' ' + $SPA.lastName;
 
    document.getElementById('userName').innerHTML = userName;
+
+  
+
+   Visualforce.remoting.Manager.invokeAction(
+   		$SPA.jsr.getUsers,
+   		function(result,event){
+   			if(event.status){
+   				for(var i=0;i<result.length;i++){
+   					 addUser(result[i]);
+   				}
+   				//document.getElementById('users').innerHTML = JSON.stringify(result, null, '\t');
+   			}else{
+   				alert(event.message);
+   			}
+   		}
+   	);
+
+   function addUser(user){
+   		var userList = document.getElementById('userList');
+   		var userItem = document.createElement('li');
+   		var img  = document.createElement('img');
+
+   		img.src = user.FullPhotoUrl;
+   		img.className = 'user';
+
+   		var span = document.createElement('span');
+   		span.innerHTML = user.Name;
+
+
+   		var aTag = document.createElement('a');
+   		aTag.setAttribute('href', '/' + user.Id);
+   		aTag.appendChild(img);
+
+   		aTag.appendChild(span);
+   		userItem.appendChild(aTag);
+
+   		userList.appendChild(userItem);
+   }
    
 })();
